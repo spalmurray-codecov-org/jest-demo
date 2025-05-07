4ca34f355b8ae873846241bba5c858f12f0107db
Spencer Murray <spencer.murray@sentry.io>
2025-05-07 14:12:31 -0400

    test just clover

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,13 @@ module.exports = {
     collectCoverage: true,
     coverageReporters: [
       'clover',
-      'text'
-    ],
-    detectOpenHandles: true,
-    preset: 'ts-jest',
-    reporters: [
-        'default',
-        'jest-junit',
-    ],
-    testEnvironment: 'jest-environment-jsdom',
-    testPathIgnorePatterns: ['node_modules', '.next', 'fileMock.js'],
+    ]
+    // detectOpenHandles: true,
+    // preset: 'ts-jest',
+    // reporters: [
+    //     'default',
+    //     'jest-junit',
+    // ],
+    // testEnvironment: 'jest-environment-jsdom',
+    // testPathIgnorePatterns: ['node_modules', '.next', 'fileMock.js'],
 };
